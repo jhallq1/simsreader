698058b461a0b8355780860b8475264417ad6164
Jennifer Nguyen <jnhallquist@gmail.com>
2016-11-29 12:36:03 -0800

    Add test to validate comment form data

---
+++ api/db/stories/util/commentFormValidator.mocha.js
@@ -0,0 +1,57 @@
+const expect = require('chai').expect,
+      validator = require('validator');
+
+data = {};
+msg = "";
+response = {};
+
+function formCompleted(data) {
+  if (validator.isNull(data.comment) && (validator.isNull(data.rating))) {
+    response.incomplete = "Both fields empty";
+    return;
+  }
+  return "Form is valid";
+}
+
+function commentValid(data) {
+  if (validator.isLength(data.comment, {min: 0, max: 250})) {
+    msg = "Comment field is valid";
+  } else {
+    response.invalidComment = "Comment cannot contain more than 250 characters";
+  }
+  return;
+}
+
+describe('Validates comment form data', function() {
+  beforeEach(function() {
+    data = {
+      comment: "Pol Roger",
+      rating: 1
+    };
+
+    msg = "";
+    response = {};
+  });
+
+  it('verifies at least one field has data', function() {
+    expect(formCompleted(data)).to.equal("Form is valid");
+  });
+
+  it('throws error both fields are empty', function() {
+    data.comment = "";
+    data.rating = "";
+    formCompleted(data);
+    expect(response.incomplete).to.equal("Both fields empty");
+  });
+
+  it('verifies that text is of valid length', function() {
+    commentValid(data);
+    expect(msg).to.equal("Comment field is valid");
+  });
+
+  it('throws error if text is too long', function() {
+    data.comment = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    commentValid(data);
+    expect(response.invalidComment).to.equal("Comment cannot contain more than 250 characters");
+  });
+});
